774469ed4cea3aab6dabe673153ffd7969a6f6c8
Toàn Trần <toantran3110@gmail.com>
2021-08-30 18:38:30 +0700

    apply sushi legacy for setting up front-end

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useEffect, useState } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
+import { UseWalletProvider } from 'use-wallet'
+import DisclaimerModal from './components/DisclaimerModal'
+import MobileMenu from './components/MobileMenu'
+import TopBar from './components/TopBar'
+import FarmsProvider from './contexts/Farms'
+import ModalsProvider from './contexts/Modals'
+import TransactionProvider from './contexts/Transactions'
+import SushiProvider from './contexts/SushiProvider'
+import useModal from './hooks/useModal'
+import theme from './theme'
+import Farms from './views/Farms'
+import Home from './views/Home'
+import Stake from './views/Stake'
 
-function App() {
+const App: React.FC = () => {
+  const [mobileMenu, setMobileMenu] = useState(false)
+
+  const handleDismissMobileMenu = useCallback(() => {
+    setMobileMenu(false)
+  }, [setMobileMenu])
+
+  const handlePresentMobileMenu = useCallback(() => {
+    setMobileMenu(true)
+  }, [setMobileMenu])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to toan.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Providers>
+      <Router>
+        <TopBar onPresentMobileMenu={handlePresentMobileMenu} />
+        <MobileMenu onDismiss={handleDismissMobileMenu} visible={mobileMenu} />
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/farms">
+            <Farms />
+          </Route>
+          <Route path="/staking">
+            <Stake />
+          </Route>
+        </Switch>
+      </Router>
+      <Disclaimer />
+    </Providers>
+  )
 }
 
-export default App;
+const Providers: React.FC = ({ children }) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <UseWalletProvider
+        chainId={1}
+        connectors={{
+          walletconnect: { rpcUrl: 'https://mainnet.eth.aragon.network/' },
+        }}
+      >
+        <SushiProvider>
+          <TransactionProvider>
+            <FarmsProvider>
+              <ModalsProvider>{children}</ModalsProvider>
+            </FarmsProvider>
+          </TransactionProvider>
+        </SushiProvider>
+      </UseWalletProvider>
+    </ThemeProvider>
+  )
+}
+
+const Disclaimer: React.FC = () => {
+  const markSeen = useCallback(() => {
+    localStorage.setItem('disclaimer', 'seen')
+  }, [])
+
+  const [onPresentDisclaimerModal] = useModal(
+    <DisclaimerModal onConfirm={markSeen} />,
+  )
+
+  useEffect(() => {
+    const seenDisclaimer = true // localStorage.getItem('disclaimer')
+    if (!seenDisclaimer) {
+      onPresentDisclaimerModal()
+    }
+  }, [])
+
+  return <div />
+}
+
+export default App
